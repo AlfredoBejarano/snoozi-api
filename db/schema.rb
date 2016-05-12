2877cc900651fface5ee04e377def251d7f0a2fa
Juan Alfredo Corona Bejarano <alfredocbejarano@gmail.com>
2016-05-12 11:43:02 -0500

    Attaches image to Dog model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512161910) do
+ActiveRecord::Schema.define(version: 20160512162519) do
 
   create_table "dogs", force: :cascade do |t|
     t.string   "breed"
@@ -21,8 +21,12 @@ ActiveRecord::Schema.define(version: 20160512161910) do
     t.date     "found_date"
     t.text     "description"
     t.string   "number"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
