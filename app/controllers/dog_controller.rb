1a7c73cfdb69f5fcd9621e286c638161028727f7
Juan Alfredo Corona Bejarano <alfredocbejarano@gmail.com>
2016-05-12 13:19:17 -0500

    Adds permited params to Dog model

--- app/controllers/dog_controller.rb
+++ app/controllers/dog_controller.rb
@@ -12,9 +12,27 @@ class DogController < ApplicationController
     render :json => Dog.all
   end
 
+  def create
+    @Dog = Dog.new(dog_params)
+
+    respond_to do |format|
+      if @Dog.save
+        format.json { render json: @Dog, status: :created }
+      else
+        format.json { render json: @Dog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def show 
     $id = sanitize(params[:id])
     @Dog = Dog.find($id)
     render :json => @Dog
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:breed, :color, :gender, :found_location, :found_date, :description, :number, :image)
   end
 end
