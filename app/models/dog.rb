a3d46dbc93f16e4656ca5d151a980b15a5d18037
Juan Alfredo Corona Bejarano <alfredocbejarano@gmail.com>
2016-05-12 11:40:22 -0500

    Attaches image to Dog model

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,18 @@
 class Dog < ActiveRecord::Base
+  has_attached_file :image, 
+  styles: {
+    thumb: ["196x196#", :jpg], 
+    original: ['512x512>', :jpg] 
+  },
+  convert_options: {
+    thumb: '-quality 75 -strip',
+    original: '-quality 85 -strip' 
+  },
+  url: '/system/:hash.:extension',
+  hash_secret: 'doge18'
+
+  validates_attachment :image, presence: true,
+  content_type: {
+    content_type: ['image/jpeg', 'image/jpg', 'image/png'] },
+  size: { in: 0..1024.kilobytes }   
 end
