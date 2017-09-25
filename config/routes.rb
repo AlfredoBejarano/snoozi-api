306551f2cad49f9dfa39f41173e8ac1501ca60e4
Alfredo Bejarano <alfredocbejarano@gmail.com>
2017-09-25 02:17:28 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :dogs
   root :to => 'dogs#index'
   match '*any' => 'application#options', :via => [:options]
+  resources :dogs, defaults: { format: 'json' }
 end
